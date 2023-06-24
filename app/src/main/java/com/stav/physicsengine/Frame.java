d35b1ef59f1aaed8b50e7f4a050d7afa890d7402
Circle1874 <stavko07@gmail.com>
2023-06-24 19:55:06 +0300

    yay

---
+++ app/src/main/java/com/stav/physicsengine/Frame.java
@@ -0,0 +1,16 @@
+package com.stav.physicsengine;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class Frame extends ViewGroup {
+    public Frame(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
